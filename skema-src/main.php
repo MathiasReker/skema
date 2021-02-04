4f0cdc403c0070c599b310719f5177f8cc719293
Mathias Reker <mathias@reker.dk>
2021-02-04 23:07:35 +0100

    First version

---
+++ skema-src/main.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use ICal\ICal;
+
+\date_default_timezone_set('Europe/Copenhagen');
+
+$ical = new ICal(__DIR__ . '/stud.kea.dk.ics', [
+    'defaultSpan' => 0,
+    'defaultTimeZone' => 'Europe/Copenhagen',
+    'defaultWeekStart' => 'MO',
+    'disableCharacterReplacement' => false,
+    'filterDaysAfter' => null,
+    'filterDaysBefore' => null,
+    'skipRecurrence' => false,
+]);
+
+$array = \json_decode(\json_encode($ical), true);
+
+$out = [];
+for ($days = 0; $days <= (int) \implode('', \getopt('d:')); ++$days) {
+    $today = \date('d-m-Y', \strtotime('+' . $days . ' day'));
+    foreach ($array['cal']['VEVENT'] as $key) {
+        if ($today === \date('d-m-Y', \strtotime($key['DTSTART_tz']))) {
+            $out[] = [
+                "\033[97mFag: \033[96m" . \substr($key['SUMMARY'], 0, \strpos($key['SUMMARY'], ' &lt;')),
+                "\033[97mLokation: \033[96m" . $key['LOCATION'],
+                "\033[97mStart: \033[96m" . \date('d-m-Y H:i:s', \strtotime($key['DTSTART_tz'])),
+                "\033[97mSlut: \033[96m" . \date('d-m-Y H:i:s', \strtotime($key['DTEND_tz'])),
+            ];
+        }
+    }
+}
+
+foreach ($out as $key => $part) {
+    $sort[$key] = \strtotime($part[0]);
+}
+
+\array_multisort($sort, \SORT_ASC, $out);
+
+foreach ($out as $key) {
+    echo \implode("\n", $key) . "\n\033[33m--\n\033[39m";
+}
---
+++ skema-src/vendor/composer/installed.php
@@ -0,0 +1,52 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'securitypro/securitypro',
+  ),
+  'versions' => 
+  array (
+    'dg/composer-cleaner' => 
+    array (
+      'pretty_version' => 'v2.2',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f1e72fb72a37014e421451185550750205993185',
+    ),
+    'johngrogg/ics-parser' => 
+    array (
+      'pretty_version' => 'v2.2.2',
+      'version' => '2.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '69c80471a0a99142ebc72b21c2bc084e81a7c4f4',
+    ),
+    'roave/security-advisories' => 
+    array (
+      'pretty_version' => 'dev-latest',
+      'version' => 'dev-latest',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'f7d723a10c7cb36e11430182f5813ecb1b887da0',
+    ),
+    'securitypro/securitypro' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
